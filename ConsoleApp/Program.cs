39a0d13027025758efedd5d9e5cdf5ec731e5600
Luis Mendoza <luis23luis33@gmail.com>
2024-01-12 12:27:19 -0500

    Separate services from the main class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,8 @@
 ﻿using Emgu.CV;
-using Emgu.CV.CvEnum;
-using System;
-using System.Drawing;
-using System.IO;
-using System.Media;
-using static System.Net.Mime.MediaTypeNames;
+//using static System.Net.Mime.MediaTypeNames;
+using ConsoleApp.Services;
 
-namespace VideoConverter
+namespace ConsoleApp
 {
     class Program
     {
@@ -20,42 +16,17 @@ namespace VideoConverter
             Console.WriteLine("Introduce la ruta del video de salida:");
             string rutaSalida = Console.ReadLine();
 
-            
-
+            //generar el video
             VideoCapture capture = new VideoCapture(rutaEntrada);
 
-            SaveFrames(capture, rutaSalida);
+            //private readonly framesSplit = new FrameSplitService();
+
+            //ejecutamos la función encargada
+            FrameSplitService.SaveFrames(capture, rutaSalida);
+            
 
             // Mostrar un mensaje de éxito
             Console.WriteLine("La ruta de entrada: "+rutaEntrada+" | La ruta salida: "+ rutaSalida);
-        }
-
-        static void SaveFrames(VideoCapture capture, string rutaSalida)
-        {
-            Mat frame = new Mat();
-
-            int frameNumber = 0;
-
-            while (capture.Read(frame))
-            {
-                SaveFrame(frame, System.IO.Path.Combine(rutaSalida, $"frame{frameNumber}.png"));
-                frameNumber++;
-            }
-
-            // Cerrar el VideoCapture
-            capture.Dispose();
-        }
-
-        static void SaveFrame(Mat frame, string fileName)
-        {
-            int frameNumber = 10;
-
-            // Crear un Image para almacenar el frame actual
-            //Image image = new Image(frame);
-            CvInvoke.Imwrite(fileName, frame);
-
-            // Mostrar el frame actual
-            Console.WriteLine("Frame {0} guardado", frameNumber);
         }
     }
 }
---
+++ ConsoleApp/Services/FrameSplitService.cs
@@ -0,0 +1,40 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Services
+{
+    public class FrameSplitService
+    {
+        public static void SaveFrames(VideoCapture capture, string rutaSalida)
+        {
+            Mat frame = new Mat();
+
+            int frameNumber = 0;
+
+            while (capture.Read(frame))
+            {
+                SaveFrame(frame, System.IO.Path.Combine(rutaSalida, $"frame{frameNumber}.png"));
+                frameNumber++;
+            }
+
+            // Cerrar el VideoCapture
+            capture.Dispose();
+        }
+
+        public static void SaveFrame(Mat frame, string fileName)
+        {
+            int frameNumber = 10;
+
+            // Crear un Image para almacenar el frame actual
+            //Image image = new Image(frame);
+            CvInvoke.Imwrite(fileName, frame);
+
+            // Mostrar el frame actual
+            Console.WriteLine("Frame {0} guardado", frameNumber);
+        }
+    }
+}
