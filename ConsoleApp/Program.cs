087a6084c85279de128de01ac55ef83c2fe9289f
Luis Mendoza <luis23luis33@gmail.com>
2024-01-14 15:08:55 -0500

    main

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,19 +15,25 @@ namespace ConsoleApp
             Console.WriteLine("Introduce la ruta del video de salida:");
             string outputRoute = Console.ReadLine();
 
+            //el usuario decide qué proceso quiere realizar con el video
+            Console.WriteLine("Ingresa 1 para tranformar un video a frames");
+            Console.WriteLine("Ingresa 2 para obtener las diferencias de un rostro");
+            int option = Convert.ToInt32(Console.ReadLine());
+
             //generar el video
             var capture = new VideoCapture(inputRoute);
-            //VideoCapture capture = new VideoCapture(inputRoute);
 
 
-            //ejecutamos la función encargada
-
-
-            FaceRecognitionService.FaceDifferences(capture, outputRoute);
+            //ejecutamos la funci´´on según las preferencias del usuario
+            if (option == 2)
+            {
+                FaceRecognitionService.FaceDifferences(capture, outputRoute);
+            }
+            FrameSplitService.SaveFrames(capture, outputRoute);
 
 
             // Mostrar un mensaje de éxito
-            Console.WriteLine("La ruta de entrada: "+inputRoute+" | La ruta salida: "+ outputRoute);
+            Console.WriteLine("La ruta de entrada: " + inputRoute + " | La ruta salida: " + outputRoute);
         }
     }
 }
