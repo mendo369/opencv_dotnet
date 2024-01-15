2a4decc932803b6d1415dbfbde3744f105a78fa5
Luis Mendoza <luis23luis33@gmail.com>
2024-01-14 20:38:35 -0500

    Error handling

--- ConsoleApp/Services/FaceRecognitionService.cs
+++ ConsoleApp/Services/FaceRecognitionService.cs
@@ -1,6 +1,6 @@
 ﻿
 using OpenCvSharp;
-using ConsoleApp.Services;
+using ConsoleApp.Utilities;
 
 namespace ConsoleApp.Services
 {
@@ -8,102 +8,74 @@ namespace ConsoleApp.Services
     {
         public static void FaceDifferences(VideoCapture capture, string outputRoute)
         {
-            // Cargar el clasificador de cascada de Haar para la detección de rostros
-            string rutaArchivo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Services", "haarcascade_frontalface_alt_tree.xml");
-            CascadeClassifier faceCascade = new CascadeClassifier(rutaArchivo);
-
-            //// Cargar el predictor de puntos de referencia faciales
-            //string facemarkModel = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Services", "shape_predictor_68_face_landmarks.dat");
-            //FacemarkLBF facemark = FacemarkLBF.Create();
-
-            //// Cargar el modelo entrenado (reemplaza con la ruta correcta)
-            //facemark.LoadModel(facemarkModel);
-
-
-            // Convertir el fotograma a escala de grises para mejorar la detección
-            Mat frame = new Mat();
-            int frameNumber = 0;
-
-            while (capture.Read(frame))
+            try
             {
-                // Detectar los rostros en el cuadro actual
-                //Rect[] faces = faceCascade.DetectMultiScale(frame);
-
-                Rect[] faces = faceCascade.DetectMultiScale(frame);
-
-                if (faces.Length > 0)
+                // Verificar si el VideoCapture está abierto
+                if (!capture.IsOpened())
                 {
-                    double[] differences = CalculateFacialDifferences(frame, faces[0]);
-
-                    foreach (var face in faces)
-                    {
-                        Mat faceROI = new Mat(frame, face);
-                        // Convertir la región de la cara a escala de grises
-                        Mat grayFace = new Mat();
-                        Cv2.CvtColor(faceROI, grayFace, ColorConversionCodes.BGR2GRAY);
-
-
-                        Cv2.Rectangle(frame, face, Scalar.Red, 2);
-                    }
-                    foreach (var diff in differences)
-                    {
-                        Console.WriteLine(diff);
-                    }
-                    // Guardar el fotograma solo si se detecta al menos una cara
-                    //FrameSplitService.SaveFrame(frame, System.IO.Path.Combine(outputRoute, $"frame{frameNumber}.png"));
-                    //frameNumber++;
+                    Console.WriteLine("Error: No se pudo abrir el video.");
+                    return;
                 }
+
+                // Crear el directorio de salida si no existe
+                if (!System.IO.Directory.Exists(outputRoute))
+                {
+                    Console.WriteLine($"Error: La ruta de salida '{outputRoute}' no existe.");
+                    return;
+                }
+                // Cargar el clasificador de cascada de Haar para la detección de rostros
+                string rutaArchivo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Services", "haarcascade_frontalface_alt_tree.xml");
+                CascadeClassifier faceCascade = new CascadeClassifier(rutaArchivo);
+
+                Mat frame = new Mat();
+
+                int frameNumber = 0;
+                bool faceDetected = false;
+
+                while (capture.Read(frame) && !faceDetected)
+                {
+                    //Mostramos un mensaje de consola mientras se procesa el video
+                    Console.Write("Processing... Frame {0}\r", frameNumber);
+
+                    //Detectar las caras del frame actual
+                    Rect[] faces = faceCascade.DetectMultiScale(frame);
+
+                    if (faces.Length > 0)
+                    {
+                        foreach (var face in faces)
+                        {
+                            Cv2.Rectangle(frame, face, Scalar.Red, 2);
+
+                            // Definir regiones de interés (ROIs) para los ojos, la nariz y la boca
+                            Rect roiEyesLeft = new Rect((int)(face.X + 0.15 * face.Width), (int)(face.Y + 0.3 * face.Height), (int)(0.2 * face.Width), (int)(0.15 * face.Height));
+                            Rect roiEyesRight = new Rect((int)(face.X + 0.65 * face.Width), (int)(face.Y + 0.3 * face.Height), (int)(0.2 * face.Width), (int)(0.15 * face.Height));
+                            Rect roiNose = new Rect((int)(face.X + 0.4 * face.Width), (int)(face.Y + 0.5 * face.Height), (int)(0.2 * face.Width), (int)(0.1 * face.Height));
+                            Rect roiMouth = new Rect((int)(face.X + 0.25 * face.Width), (int)(face.Y + 0.7 * face.Height), (int)(0.5 * face.Width), (int)(0.1 * face.Height));
+
+                            // Dibujar cuadros alrededor de los ojos, la nariz y la boca
+                            Cv2.Rectangle(frame, roiEyesLeft, Scalar.Blue, 2);
+                            Cv2.Rectangle(frame, roiEyesRight, Scalar.Blue, 2);
+                            Cv2.Rectangle(frame, roiNose, Scalar.Green, 2);
+                            Cv2.Rectangle(frame, roiMouth, Scalar.Yellow, 2);
+
+                            // Guardar el frame con los cuadros dibujados solo si no se ha guardado una imagen previamente
+                            if (!faceDetected)
+                            {
+                                UtilitiesOpenCV.SaveFrame(frame, outputRoute, $"rostro");
+                                faceDetected = true;  // Marcamos que ya se ha detectado y guardado una cara
+                            }
+                        }
+                    }
+                    frameNumber++;
+                }
+
+                capture.Release();
             }
-            capture.Release();
-
-            //while (capture.Read(frame))
-            //{
-
-            //    // Detectar caras en el fotograma
-            //    var faces = faceCascade.DetectMultiScale(frame);
-
-            //    if (faces.Length > 0)
-            //    {
-            //        foreach (var face in faces)
-            //        {
-            //            Mat faceROI = new Mat(frame, face);
-            //            // Convertir la región de la cara a escala de grises
-            //            Mat grayFace = new Mat();
-            //            Cv2.CvtColor(faceROI, grayFace, ColorConversionCodes.BGR2GRAY);
-
-
-            //            Cv2.Rectangle(frame, face, Scalar.Red, 2);
-            //        }
-
-            //        // Guardar el fotograma solo si se detecta al menos una cara
-            //        FrameSplitService.SaveFrame(frame, System.IO.Path.Combine(outputRoute, $"frame{frameNumber}.png"));
-            //        frameNumber++;
-            //    }
-            //}
-        }
-
-        static double[] CalculateFacialDifferences(Mat frame, Rect face)
-        {
-            // Calcular las diferencias entre las características faciales
-            double[] differences = new double[5];
-
-            // Diferencia entre la posición de los ojos
-            differences[0] = (face.X + face.Width / 2) - (face.Y + face.Height / 2);
-
-            // Diferencia entre el ancho y el alto de la cara
-            differences[1] = face.Width - face.Height;
-
-            // Diferencia entre la distancia entre los ojos y la distancia entre la nariz y la boca
-            differences[2] = (face.Y + face.Height / 2) - (face.X + face.Width / 4);
-
-            // Diferencia entre la anchura de la nariz y la anchura de la boca
-            differences[3] = face.Width / 4 - face.Width / 8;
-
-            // Diferencia entre la altura de la nariz y la altura de la boca
-            differences[4] = face.Height / 2 - face.Height / 4;
-
-            return differences;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            
         }
     }
 }
-
