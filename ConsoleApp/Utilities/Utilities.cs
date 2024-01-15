76e02b61d9bbcf6d4c74d77d1a42261ad14bfb6c
Luis Mendoza <luis23luis33@gmail.com>
2024-01-14 20:32:36 -0500

    Save frame function

---
+++ ConsoleApp/Utilities/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using OpenCvSharp;
+
+namespace ConsoleApp.Utilities
+{
+    public class UtilitiesOpenCV
+    {
+        public static bool SaveFrame(Mat frame, string outputRoute,string fileName)
+        {
+            string outputName = System.IO.Path.Combine(outputRoute, $"frame{fileName}.png");
+            try
+            {
+                // Guardar el frame actual como una imagen
+                Cv2.ImWrite(outputName, frame);
+                Console.WriteLine($"Imágen {fileName} guardada");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error al guardar: {e.Message}");
+                return false;
+            }
+        }
+    }
+    
+}
